21470b01b93c615834f2cadf781b3ada0bd384b5
Nando1702 <usuario@DESKTOP-2GA9RPP>
2023-05-17 17:57:30 +0200

    commit 1, mejora del formato de salida commit 2, programa encapsulado en metodos + resolucion de problemas

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -13,113 +13,167 @@ import java.util.Scanner;
  */
 public class Practica1 {
 
+    public static Scanner sc = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
+
         double res = 0;
         String operacion;
         boolean comprobar = false;
 
         do {
 
-            String numero1;
-            do {
-                System.out.print("Introdueix el primer numero \n>");
-                numero1 = sc.nextLine();
-            } while (!numero1.matches("[+-]?[\\d]*[.]?[\\d]+"));
-            double nume1 = Double.parseDouble(numero1);
-            double n1 = new Double(numero1);
+            double n1 = introduirDouble("primer");
 
             do {
-                System.out.print("""
+
+                menuPrincipal();
+                operacion = sc.nextLine();
+                comprobar = comprobarOperacio(operacion);
+
+            } while (comprobar != true);
+
+            double n2 = introduirDouble("segon");
+
+            do {
+                comprobar = true;
+                switch (operacion) {
+                    case "+" ->
+                        res = n2 + n2;
+                    case "-" ->
+                        res = n1 - n2;
+                    case "x", "X" ->
+                        res = n1 * n2;
+                    case "/" -> {
+
+                        if (n2 == 0) {
+
+                            n2 = introduirDoubleDiferenteA0();
+
+                        }
+
+                        res = n1 / n2;
+                    }
+                    case "*" ->
+                        res = Math.pow(n1, n1);
+                    case "%" -> {
+                        if (n2 == 0) {
+
+                            n2 = introduirDoubleDiferenteA0();
+
+                        }
+                        res = n1 % n2;
+                    }
+                }
+            } while (comprobar != true);
+
+            System.out.println("El resultat de " + n1 + " " + operacion + " " + n2 + "  és " + res);
+            System.out.print("Vols continuar operant? [s/n] \n>");
+            
+            operacion = retornarValorValidSortida();
+
+            
+        } while (operacion.equalsIgnoreCase("s") );
+    }
+
+    public static void menuPrincipal() {
+
+        System.out.print("""
                                    Indica la operacio que vols utilitzar:
                                    Suma\t[+]
                                    Resta\t[-]
-                                   Multiplicaci\u00f3\t[x][*]
+                                   Multiplicaci\u00f3\t[xX]
                                    Divisio\t[/][%]
+                                   Exponent\t[*]
+                                   Modul\t[%]
                                    >""");
-                
-                operacion = sc.nextLine();
-                if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x")
-                        || operacion.equals("X") || operacion.equals("/") || operacion.equals("%")
-                        || operacion.equals("*")) {
-                    comprobar = true;
-                } else {
-                    comprobar = false;
-                }
-            } while (comprobar != true);
 
-            String numero2;
-            do {
-                System.out.print("Introdueix el segon numero \n>");
-                numero2 = sc.nextLine();
-            } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
-            double nume2 = Double.parseDouble(numero2);
-            double n2 = new Double(numero2);
-
-            do {
-                comprobar = true;
-                switch (operacion) {
-                    case "+":
-                        res = n2 + n2;
-                        break;
-                    case "-":
-                        res = n1 - n2;
-                        break;
-                    case "x":
-                    case "X":
-                        res = n1 * n2;
-                        break;
-                    case "/":
-                        while (n2 == 0) {
-                            do {
-                                System.err.print("Error. Al denominador hi ha un zero!\n");
-                                numero2 = sc.nextLine();
-                            } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
-                            nume2 = Double.parseDouble(numero2);
-                            n2 = new Double(numero2);
-                        }
-                        res = n1 / n2;
-                        break;
-                    case "*":
-                        res = Math.pow(n1, n1);
-                        break;
-                    case "%":
-                        while (n2 == 0) {
-                            do {
-                                System.err.print("Error. Al denominador hi ha un zero!\n"
-                                        );
-                                numero2 = sc.nextLine();
-                            } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
-                            nume2 = Double.parseDouble(numero2);
-                            n2 = new Double(numero2);
-                        }
-                        res = n1 % n2;
-                        break;
-                }
-            } while (comprobar != true);
-
-            System.out.println("El resultat de " + numero1 + " " + operacion + " " + numero2 +  "  és " + res);
-            System.out.print("Vols continuar operant? [s/n] \n>");
-
-            do {
-                comprobar = true;
-                operacion = sc.nextLine();
-
-                switch (operacion) {
-                    case "s":
-                    case "S":
-                    case "n":
-                    case "N":
-                        break;
-                    default:
-                        System.err.println("\n Error, posa un valor vàlid. \n");
-                        comprobar = false;
-                }
-            } while (comprobar != true);
-        } while (operacion.equals("s") || operacion.equals("S"));
     }
+
+    public static boolean comprobarOperacio(String operacion) {
+
+        return operacion.matches("[xX+-/*%]");
+
+    }
+
+    public static double introduirDouble(String valor) {
+
+        System.out.print("Introdueix el " + valor + " numero \n>");
+
+        do {
+
+            if (sc.hasNextDouble()) {
+
+                return sc.nextDouble();
+
+            } else {
+                System.out.println("Error, Introdueix un valor possible");
+            }
+
+            sc.nextLine();
+
+        } while (true);
+
+    }
+
+    public static double introduirDoubleDiferenteA0() {
+
+        System.out.println("Error al denominador hi ha un 0");
+
+        do {
+
+            if (sc.hasNextDouble()) {
+
+                double num = sc.nextDouble();
+                if (num == 0) {
+
+                    System.out.println("Error, Introdueix un valor diferent a 0");
+                } else {
+
+                    return num;
+                }
+
+            } else {
+                System.out.println("Error, Introdueix un valor possible");
+            }
+
+            sc.nextLine();
+
+        } while (true);
+
+    }
+
+    public static boolean comprobarValorValidSortida(String valor) {
+
+        return valor.matches("[sSnN]");
+
+    }
+
+    public static String retornarValorValidSortida() {
+        
+        sc.next();
+
+        String valor = sc.nextLine();
+
+        do {
+
+            if (comprobarValorValidSortida(valor)) {
+
+                return valor;
+            
+            } else {
+                
+                System.err.print("\n Error, posa un valor vàlid \n>");
+                valor = sc.nextLine();
+            }
+            
+            
+
+        } while (true);
+
+    }
+
 }
